11cfb242a6ac321473562a129606488e1b70d120
Sion <sionn908@gmail.com>
2022-05-17 14:36:37 +0900

    add culc dist

--- location.js
+++ location.js
@@ -7,14 +7,30 @@ function getGeo(){
 }
 
 function successCallback(position) {
-  var gl_text = "緯度：" + position.coords.latitude + "<br>";
-    gl_text += "経度：" + position.coords.longitude + "<br>";
+  var nlan = position.coords.latitude;
+  var nlng = position.coords.longitude;
+  var accuracy = (position.coords.accuracy > 50 ? position.coords.accuracy : 50);
+  var dist = distance(nlan,nlng,32.73834987305667, 129.8728843611234);
+  var gl_text = "緯度：" + nlan + "<br>";
+    gl_text += "経度：" + nlng + "<br>";
     gl_text += "高度：" + position.coords.altitude + "<br>";
-    gl_text += "緯度・経度の誤差：" + position.coords.accuracy + "<br>";
+    gl_text += "緯度・経度の誤差：" + accuracy + "<br>";
     gl_text += "高度の誤差：" + position.coords.altitudeAccuracy + "<br>";
     gl_text += "方角：" + position.coords.heading + "<br>";
     gl_text += "速度：" + position.coords.speed + "<br>";
+    gl_text += "目的地との誤差：" + dist + "<br>";
+    gl_text += "圏内にいるか：" + dist < accuracy + "<br>";
   document.getElementById("show_result").innerHTML = gl_text;
+}
+
+const R = Math.PI / 180;
+
+function distance(lat1, lng1, lat2, lng2) {
+  lat1 *= R;
+  lng1 *= R;
+  lat2 *= R;
+  lng2 *= R;
+  return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2))*1000;
 }
 
 /***** 位置情報が取得できない場合 *****/
